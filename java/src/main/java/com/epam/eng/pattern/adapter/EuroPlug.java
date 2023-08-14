a8a29071086e43258dc8086dc6b3c4efd3214062
Dennis Xie <dennisxie1992@outlook.com>
2023-08-14 17:30:51 +0800

    Refactor the whole adapter pattern problem

---
+++ java/src/main/java/com/epam/eng/pattern/adapter/EuroPlug.java
@@ -0,0 +1,9 @@
+package com.epam.eng.pattern.adapter;
+
+public class EuroPlug implements IEuroPlug {
+
+    @Override
+    public void supplyPower(int voltage, int frequency) {
+        System.out.println(String.format("Start to supply electricity of %d volts and %d Hz", voltage, frequency));
+    }
+}
---
+++ java/src/main/java/com/epam/eng/pattern/adapter/thirdparty/IAmericanPlug.java
@@ -0,0 +1,5 @@
+package com.epam.eng.pattern.adapter.thirdparty;
+
+public interface IAmericanPlug {
+    void supplyCommercialElectricity(int voltage, int frequency);
+}
