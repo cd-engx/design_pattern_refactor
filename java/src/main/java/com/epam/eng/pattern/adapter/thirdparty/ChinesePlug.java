bb6c0ce27af3dcb1b27a535d42e73573841a43d9
Dennis Xie <dennisxie1992@outlook.com>
2023-08-16 11:05:32 +0800

    Let ChinsesPlug has more methods

--- java/src/main/java/com/epam/eng/pattern/adapter/thirdparty/ChinesePlug.java
+++ java/src/main/java/com/epam/eng/pattern/adapter/thirdparty/ChinesePlug.java
@@ -1,8 +1,23 @@
 package com.epam.eng.pattern.adapter.thirdparty;
 
 public class ChinesePlug implements IChinesePlug{
+    private int frequency;
+    private int voltage;
+
     @Override
-    public void supplyPower(int voltage) {
-        System.out.println("Supplying power with " + voltage + "V");
+    public void setPowerFrequency(int frequency) {
+        this.frequency = frequency;
+        System.out.println("Power frequency is set to " + frequency + "Hz");
+    }
+
+    @Override
+    public void setPowerVoltage(int voltage) {
+        this.voltage = voltage;
+        System.out.println("Power voltage is set to " + voltage + "V");
+    }
+
+    @Override
+    public void supplyPower() {
+        System.out.println("Supplying power with " + voltage + "V" + frequency + "Hz");
     }
 }
