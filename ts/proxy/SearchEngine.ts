df1c955071a34ebc8854ee146bcfccd856b3521e
Kevin_Cai <Kevin_Cai@epam.com>
2023-08-23 14:07:51 +0800

    refactor: proxy

--- ts/proxy/SearchEngine.ts
+++ ts/proxy/SearchEngine.ts
@@ -1,3 +1,9 @@
+/**
+ * In the given code, the GoogleSearchEngine class performs a search operation for the given query string. 
+ * To add a proxy, you can create a GoogleSearchEngineProxy class that implements the same SearchEngine interface.
+ * In this example, I will add caching functionality to the proxy so that previously searched queries can return cached results without requiring another search.
+ */
+
 interface SearchEngine {
     search(query: string): string;
 }
@@ -9,5 +15,41 @@ class GoogleSearchEngine implements SearchEngine {
     }
 }
 
-var searchEngine: GoogleSearchEngine = new GoogleSearchEngine();
-var result = searchEngine.search("NEWS");
+class GoogleSearchEngineProxy implements SearchEngine {
+    private searchEngine: GoogleSearchEngine;
+    private cache: Map<string, string>;
+
+    constructor() {
+        this.searchEngine = new GoogleSearchEngine();
+        this.cache = new Map<string, string>();
+    }
+
+    search(query: string): string {
+        if (this.cache.has(query)) {
+            console.log(`Returning cached results for query: ${query}`);
+            return this.cache.get(query) as string;
+        }
+
+        const results = this.searchEngine.search(query);
+        this.cache.set(query, results);
+        return results;
+    }
+}
+
+// Usage
+const searchEngineProxy = new GoogleSearchEngineProxy();
+
+let result = searchEngineProxy.search("NEWS");
+console.log(result);
+
+// The second search query will return cached results without performing the search again.
+result = searchEngineProxy.search("NEWS");
+console.log(result);
+
+/**
+ * GoogleSearchEngineProxy acts as a proxy for the GoogleSearchEngine class. 
+ * It handles caching functionality, 
+ * improving efficiency by avoiding calling the actual search operation for previously searched queries. 
+ * The client (the searchEngineProxy object) is now working with the proxy, 
+ * which provides additional functionality without changing the GoogleSearchEngine class.
+ */
