9451a6e44dcf54d3ee7da70aee5ca0bdf1151405
Kevin_Cai <Kevin_Cai@epam.com>
2023-08-23 14:03:27 +0800

    refactor: state

--- ts/state/TrafficSignal.ts
+++ ts/state/TrafficSignal.ts
@@ -1,49 +1,85 @@
-class TrafficSignal {
-    private state: number;
+/**
+ * To refactor this code using the State pattern,
+ * you can create a SignalState interface and implement concrete state classes for each state (Red, Green, Yellow).
+ * The TrafficSignal class will then delegate state-specific behavior to these state classes.
+ */
 
-    constructor() {
-        this.state = 0;
+// Define the SignalState interface
+interface SignalState {
+    runSignal(trafficSignal: TrafficSignal): Promise<void>;
+    next(): void;
+}
+
+// Implement the concrete state classes for Red, Green, and Yellow lights
+class RedLight implements SignalState {
+    async runSignal(trafficSignal: TrafficSignal): Promise<void> {
+        console.log("Red Light for 30 seconds...");
+        await trafficSignal.sleep(3000);
+        trafficSignal.next();
     }
 
-    public setState(state: number): void {
+    next(): void {
+        trafficSignal.setState(new GreenLight());
+    }
+}
+
+class GreenLight implements SignalState {
+    async runSignal(trafficSignal: TrafficSignal): Promise<void> {
+        console.log("Green Light for 60 seconds...");
+        await trafficSignal.sleep(6000);
+        trafficSignal.next();
+    }
+
+    next(): void {
+        trafficSignal.setState(new YellowLight());
+    }
+}
+
+class YellowLight implements SignalState {
+    async runSignal(trafficSignal: TrafficSignal): Promise<void> {
+        console.log("Yellow Light for 10 seconds...");
+        await trafficSignal.sleep(1000);
+        trafficSignal.next();
+    }
+
+    next(): void {
+        trafficSignal.setState(new RedLight());
+    }
+}
+
+// Update the TrafficSignal class to work with SignalState objects
+class TrafficSignal {
+    private state: SignalState;
+
+    constructor() {
+        this.state = new RedLight();
+    }
+
+    public setState(state: SignalState): void {
         this.state = state;
+    }
+
+    public sleep(timeout: number) {
+        return new Promise(resolve => setTimeout(resolve, timeout));
     }
 
     public async runSignal() {
         while (true) {
-            switch (this.state) {
-                case 0:
-                    console.log("Red Light for 30 seconds...");
-
-                    await this.sleep(3000);
-                    this.setState(1);
-
-                    break;
-                case 1:
-                    console.log("Green Light for 60 seconds...");
-
-                    await this.sleep(6000);
-                    this.setState(2);
-
-                    break;
-                case 2:
-                    console.log("Yellow Light for 10 seconds...");
-
-                    await this.sleep(1000);
-                    this.setState(0);
-                    
-                    break;
-                default:
-                    console.error("Invalid state");
-                    break;
-            }
+            await this.state.runSignal(this);
         }
     }
 
-    async sleep(timeout: number) {
-        return new Promise(resolve => setTimeout(resolve, timeout));
+    public next(): void {
+        this.state.next();
     }
 }
 
+// Usage is the same
 var trafficSignal = new TrafficSignal();
 trafficSignal.runSignal();
+
+/**
+ * Now, the TrafficSignal class delegates state-specific behavior to state classes that implement the SignalState interface.
+ * This makes the code more flexible and extensible,
+ * as adding new states or modifying existing states becomes easier without affecting the TrafficSignal class.
+ */
