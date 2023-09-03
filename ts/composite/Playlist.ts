fb073efe41ab6ffb6c42abfd88ebf6ff6a9e8734
Kevin_Cai <Kevin_Cai@epam.com>
2023-09-03 14:38:25 +0800

    refactor: composite

--- ts/composite/Playlist.ts
+++ ts/composite/Playlist.ts
@@ -1,47 +1,57 @@
- class Playlist {
-    
+interface IPlayable {
+    play(): void;
 }
 
- class Song {
-    private name: string;
-    private artist: string;
-    private duration: number;
+class Playlist implements IPlayable {
+    private playables: IPlayable[];
 
-    constructor(name: string, artist: string, duration: number) {
-        this.name = name;
-        this.artist = artist;
-        this.duration = duration;
+    constructor() {
+        this.playables = [];
     }
 
-    public getName(): string {
-        return this.name;
+    public add(playable: IPlayable): void {
+        this.playables.push(playable);
     }
 
-    public setName(name: string): void {
-        this.name = name;
+    public remove(playable: IPlayable): void {
+        const index = this.playables.indexOf(playable);
+        if (index >= 0) {
+            this.playables.splice(index, 1);
+        }
     }
 
-    public getArtist(): string {
-        return this.artist;
+    public play(): void {
+        console.log(`Playing Playlist:`);
+        for(const playable of this.playables) {
+            playable.play();
+        }
+    }
+}
+
+class Song implements IPlayable {
+    constructor(private name: string, private artist: string, private duration: number) {
     }
 
-    public setArtist(artist: string): void {
-        this.artist = artist;
-    }
-
-    public getDuration(): number {
-        return this.duration;
-    }
-
-    public setDuration(duration: number): void {
-        this.duration = duration;
-    }
+    // Getters and setters...
 
     public play(): void {
         console.log(`Playing song '${this.name}' by ${this.artist} (${this.duration} seconds)`);
     }
 }
 
-var playList = new Playlist();
-//playList.play();
+const song1 = new Song("Song1", "Artist1", 180);
+const song2 = new Song("Song2", "Artist2", 200);
+const song3 = new Song("Song3", "Artist3", 240);
 
+const playlist1 = new Playlist();
+playlist1.add(song1);
+playlist1.add(song2);
+
+const playlist2 = new Playlist();
+playlist2.add(song3);
+
+const mainPlaylist = new Playlist();
+mainPlaylist.add(playlist1);
+mainPlaylist.add(playlist2);
+
+mainPlaylist.play();
